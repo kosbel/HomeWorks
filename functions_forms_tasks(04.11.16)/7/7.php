dc1f2f7fe6577b50274c42bc6ca52a32c95601cc
Kostya Belyi <Kbelyi@list.ru>
2016-11-13 03:17:48 +0200

    Task 7

---
+++ functions_forms_tasks(04.11.16)/7/7.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<?php
+
+if ($_POST)
+{
+    $text_comment = htmlspecialchars($_POST['comment']);
+
+    $f = fopen('comments.txt', 'a');
+    fwrite($f, "<p style=\"width: 400px; margin: 0 auto;\">" . $text_comment . "</p>" . PHP_EOL . PHP_EOL);
+    fclose($f);
+
+    $text = file_get_contents('comments.txt');
+
+    echo $text;
+}
+
+?>
+
+<form action="" method="post" style="width: 400px; margin: 0 auto;">
+    <label>
+        <p>Комментарий:</p>
+        <textarea name="comment" cols="50" rows="10"></textarea>
+    </label>
+
+    <p><input type="submit"></p>
+</form>
+
+</body>
+</html>
+
